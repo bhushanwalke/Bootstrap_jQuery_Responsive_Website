ad7f16f252f51d85b6aa6ce338f7371f3c708673
bhushanwalke <bhushan.h2@gmail.com>
2015-10-31 12:38:26 -0700

    Landing Page

---
+++ api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'taH3bdXf9YsNJvIfUH7UcR5xT');
+    define('CONSUMER_SECRET', 'o6gh2s9yrWdLhAJjCSWcdDtRIF9knsNKybzARSkFdqr2CzkxiY');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '758879257-TyYDjECoQWCnq903cCDQcJZkQ7RLOQcnoQZiXO2x');
+    define('ACCESS_SECRET', 'HTgjNPB4jmNIr3YLTe3phGAa9sklajM2Z7q0dkHwMSNR8');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
